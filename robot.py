51a3b3d868325698c3d8425026fcf18caa48e021
dragonman750 <rowanrw@gmail.com>
2015-01-05 21:33:02 -0500

    Robot controls
    Makes robot walk forward, back, blow up evil box. Made at HATCH WFPL.

---
+++ robot.py
@@ -0,0 +1,17 @@
+class Robot:
+    def __init__(self, name, arms, legs, bomb): 
+        self.name = name 
+        self.arms = arms 
+        self.legs = legs
+        self.bomb = bomb
+        self.pos = 0
+
+    def explod(self, thing):
+        print("i exploded")
+        print(thing)
+    
+    def walk(self):
+        self.pos += 1
+      
+    def walkbackword(self):
+        self.pos -= 1
